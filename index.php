b6b1b2a1a06a8bbbfecc34e00880c3310c6a1db5
Marcelo Garcia <marcelogarcia000@hotmail.com>
2022-03-29 09:02:43 -0300

    variables php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Variavéis em PHP</h1>
+
+    <?php
+        $text = 'values of the variable is:';
+
+        $value_int = 10;
+        echo "$text $value_int";
+
+        $value_float = 10.4;
+        echo "$text $value_float";
+
+        $boolean = true;
+        echo "$text $boolean";
+
+    ?>
+</body>
+</html>
